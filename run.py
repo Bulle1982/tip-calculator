5f6ffc5899aed44a68729429e241112f494f19e7
Bulle1982 <bullites114@hotmail.com>
2023-12-10 22:39:40 +0000

    Tip Calculator Code

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to the tip calculator.")
+
+bill = float(input("What was the total bill? $"))
+tip_num = float(input("What percentage tip would you like to give? 10, 12, or 15?"))
+number_of_persons = int(input("How many people to split the bill?"))
+
+tip_prc = tip_num / 100
+tip = bill * tip_prc
+total_bill = bill + tip
+bill_per_person = round(total_bill / number_of_persons, 2)
+print(f"Each person should pay: ${bill_per_person}")
+
+
